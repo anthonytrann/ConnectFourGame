f226108fde2708de32e3df984bdb4e6cdebebd01
Anthony Tran <anthonytran@MacBookAir.broadband>
2024-12-09 22:25:44 +0000

    makes print board more readable

--- src/gameplay.py
+++ src/gameplay.py
@@ -2,6 +2,7 @@ from src.connect_four_game import ConnectFourGame
 from pprint import pprint
 import os
 
+
 def main():
     game = ConnectFourGame()
     print("\nWelcome to Connect Four!\n")
@@ -9,33 +10,50 @@ def main():
     is_full = False
     while winner is False and is_full is False:
         message = ""
-        print("############################################\n")
-        pprint(game.get_board)
-        print("   0  ,  1  ,  2  ,  3  ,  4  ,  5  ,  6  \n")
-        position = input(f"Player '{game.get_player}' Please enter the position you would like to play: \n")
-        try: 
-            message = game.play(int(position))
+        print("############################################################\n")
+        print_board(game.get_board)
+
+        position = input(
+            f"Player '{game.get_player.strip()}' Please enter the position you would like to play: \n"
+        )
+        try:
+            message = game.play(int(position) - 1)
         except (TypeError, ValueError):
-            print("############################################\n\n",f"INVALID '{position}' Please input a number between 0 and {game._max_width}\n")
+            print(
+                "############################################################\n\n",
+                f"INVALID '{position}' Please input a number between 0 and {game._max_width}\n",
+            )
         if message:
             print(message)
         winner = game.check_winner
         is_full = game.check_board_is_full
 
     if winner is not False:
-        pprint(game.get_board)
-        print("   0  ,  1  ,  2  ,  3  ,  4  ,  5  ,  6  \n") 
-        print(f"Congraulations! The winners is : {winner}")
+        print_board(game.get_board)
+        print(f"Congraulations! The winners is : {winner.strip()}")
     else:
-        pprint(game.get_board)
-        print("   0  ,  1  ,  2  ,  3  ,  4  ,  5  ,  6  \n") 
+        print_board(game.get_board)
         print("There is no winner")
 
 
+def print_board(board):
+    counter = 0
+    for row in board:
+        row_str = "|"
+        for item in row:
+            if not item:
+                row_str += "  None |"
+            else:
+                row_str += f"  {item}   |"  
+        print("------------------------------------------------------------")
+        print(row_str, counter)
+        counter += 1
+    print("============================================================")
+    print("|   1   |   2   |   3   |   4   |   5   |   6   |   7   |\n")
+
 
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
-
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 if __name__ == "__main__":
